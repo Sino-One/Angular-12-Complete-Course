77c51fcf9a0b15d68b7f92179741dbeb4b330ad4
Jerome MASSON <jerome.masson@cbainfo.fr>
2021-09-10 14:45:26 +0200

    loading indicator

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import {Post} from "./post.model";
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -26,7 +27,7 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
@@ -34,6 +35,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http.get<{[key: string]: Post}>('https://angular-http-full-course-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
       .pipe(
       //  tap(response => console.log(response)),
@@ -48,8 +50,8 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe( posts => {
-      console.log(posts);
-      this.loadedPosts = posts;
+this.isFetching = false;
+this.loadedPosts = posts;
     });
   }
 }
