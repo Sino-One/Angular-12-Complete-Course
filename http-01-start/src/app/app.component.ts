cdf011190c5e081af1945dec6a4f07feb28ad6bf
Jerome MASSON <jerome.masson@cbainfo.fr>
2021-09-10 14:30:55 +0200

    first method typing Post

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map, tap} from "rxjs/operators";
+import {Post} from "./post.model";
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     this.http.post(
       'https://angular-http-full-course-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
       postData).subscribe((responseData) => {
@@ -36,8 +37,8 @@ export class AppComponent implements OnInit {
     this.http.get('https://angular-http-full-course-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
       .pipe(
       //  tap(response => console.log(response)),
-        map(responseData => {
-          const postsArray = [];
+        map((responseData: {[key: string]: Post}) => {
+          const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
               postsArray.push({...responseData[key], id: key});
@@ -47,7 +48,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe( posts => {
-      console.log(posts);
+      console.log(posts[0]);
     });
   }
 }
