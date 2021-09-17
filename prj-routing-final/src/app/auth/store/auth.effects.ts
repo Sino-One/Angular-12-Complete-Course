923e07a86bc3e8ec07e2c0e33c69398f43769658
Jerome MASSON <jerome.masson@cbainfo.fr>
2021-09-17 15:51:23 +0200

    [DONT COMPILE] effect and error handling

--- prj-routing-final/src/app/auth/store/auth.effects.ts
+++ prj-routing-final/src/app/auth/store/auth.effects.ts
@@ -1,12 +1,40 @@
 import * as AuthActions from './auth.actions';
-import {Actions, ofType} from '@ngrx/effects';
+import {Actions, ofType, Effect} from '@ngrx/effects';
+import {catchError, switchMap, map} from 'rxjs/operators';
+import {AuthresponseData} from "../auth.service";
+import {HttpClient} from "@angular/common/http";
+import {of} from "rxjs";
+
+export interface AuthresponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 export class AuthEffects {
-
+  @Effect()
   authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthresponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAOecvVFggXeJvRBmYNSMA3eW9fxdRLiT0',
+        {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true
+        }).pipe(catchError(error => {
+          // ...
+        of();
+      }), map(resData => {
+        of();
+      }));
+    }),
+
   );
 
-  constructor(private actions$: Actions) { // Dollar sign because is Observable
+  constructor(private actions$: Actions, private http: HttpClient) { // Dollar sign because is Observable
   }
 }
