9000f34063d9cac5a3609320b0025a16d018307d
Jerome MASSON <jerome.masson@cbainfo.fr>
2021-09-20 17:24:26 +0200

    add, update and delete recipes with ngrx

--- ngrx-09-devtools-and-routing/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ngrx-09-devtools-and-routing/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,6 +6,7 @@ import { RecipeService } from '../recipe.service';
 import * as fromApp from '../../store/app.reducer';
 import { Store } from '@ngrx/store';
 import { map, switchMap } from 'rxjs/operators';
+import * as RecipesActions from '../store/recipe.action';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -47,7 +48,8 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
+  //  this.recipeService.deleteRecipe(this.id);
+    this.store.dispatch(new RecipesActions.DeleteRecipe(this.id));
     this.router.navigate(['/recipes']);
   }
 
