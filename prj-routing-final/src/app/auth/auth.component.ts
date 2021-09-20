af7448fbe4c669a058654088bc67e147acb0466d
Jerome MASSON <jerome.masson@cbainfo.fr>
2021-09-20 11:55:04 +0200

    clean auth code

--- prj-routing-final/src/app/auth/auth.component.ts
+++ prj-routing-final/src/app/auth/auth.component.ts
@@ -1,11 +1,9 @@
 import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthresponseData, AuthService} from './auth.service';
-import {Observable, Subscription} from 'rxjs';
-import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 import {AlertComponent} from '../shared/alert/alert.component';
 import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
-import {Store} from "@ngrx/store";
+import {Store} from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
 
@@ -22,9 +20,7 @@ export class AuthComponent implements OnDestroy, OnInit {
   private closeSub: Subscription;
   private storeSub: Subscription;
 
-  constructor(private authService: AuthService,
-              private router: Router,
-              private CFResolver: ComponentFactoryResolver,
+  constructor(private CFResolver: ComponentFactoryResolver,
               private store: Store<fromApp.AppState>) {
   }
 
